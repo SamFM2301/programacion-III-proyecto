bdc8ac298db5458ec2aecbb11167526eb728a07d
SamuelFrias <efrias_24@alu.uabcs.mx>
2026-04-28 20:58:42 -0700

    Agregar las librerias jackson al proyecto

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,40 +11,49 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import models.UserModel;
 
 public class UserRepository {
-	private final String FILE = System.getProperty("user.dir") + "/src/assets/files/users.csv";
+	private final String FILE = System.getProperty("user.dir") + "/src/assets/files/users.json";
 
+	private final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUT);
+	
     public void save(UserModel user) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILE, true), StandardCharsets.UTF_8))) {
-            writer.write(user.toCsv());
-            writer.newLine();
-        }
+        List<UserModel> users = getUsers();
+        users.add(user);
+        updateAll(users);
     }
-    
 
     // LEER A LOS USUARIOS
     public List<UserModel> getUsers() throws IOException {
-        List<UserModel> users = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                UserModel user = UserModel.fromCsv(line);
-                users.add(user);
-            }
-        }
-
-        return users;
+    	File file = new File(FILE);
+    	
+    	if(!file.exists() || file.length() == 0) {
+    		return new ArrayList<>();
+    	}
+    	
+    	return mapper.readValue(
+			file, 
+			new TypeReference<List<UserModel>>() {}
+		);
     }
+   
     
     public void delete(String email) throws IOException {
         List<UserModel> users = getUsers();
         users.removeIf(u -> u.getEmail().equals(email));
         rewriteFile(users);
     }
-
+    
+    public void updateAll(List<UserModel> users) throws IOException {
+    	mapper.writeValue(new File(FILE), users);
+    }
+    
     public void update(String originalEmail, UserModel updatedUser) throws IOException {
         List<UserModel> users = getUsers();
         for (int i = 0; i < users.size(); i++) {
